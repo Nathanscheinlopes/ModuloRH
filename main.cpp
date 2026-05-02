6363759fad5194e4320ceb97dc06be362b8dfe73
mbraunn04 <mbraunn04@gmail.com>
2026-05-01 21:45:26 -0300

    INCLUSÃO DA CLASSE CARGOS E OUTRAS REVISÕES

--- main.cpp
+++ main.cpp
@@ -12,6 +12,79 @@
 #include <algorithm>
 
 using namespace std;
+
+    class Cargo // para a segunda avaliacao, implementar a opcao de cadastrar mais cargos ou editar os existentes
+    {
+        private:
+
+            int id_cargo;
+            string nome;
+            float salarioBase;
+
+        public:
+            
+            Cargo() // metodo construtor sem parametros
+            {
+                this->id_cargo = 0;
+                this->nome = "Nao preenchido";
+                this->salarioBase = 0;
+            }
+
+            Cargo(int id_cargo_arq, string nome_arq, float salario_arq) // metodo construtor para o metodo extrairCargosPlanilha - informacoes extraidas de arquivo csv
+            {
+                this->id_cargo = id_cargo_arq;
+                this->nome = nome_arq;
+                this->salarioBase = salario_arq;
+            }
+
+            void extrairCargosPlanilha(vector <Cargo>& cargos)
+            {
+                ifstream arquivo("TabelaCargos.csv"); // ifstream le dados no arquivo
+                if (arquivo.is_open()) // confere se o arquivo esta aberto
+                {
+                    string linha; // variavel auxiliar para salvar as linhas lidas do arquivo
+                    getline (arquivo, linha); // copia a primeira linha de cabecalho (eliminar)
+
+                    while (getline(arquivo, linha)) 
+                    {
+                        string dados_cargos[3]; // Vetor temporário para guardar os pedaços da linha
+                        size_t pos = 0;
+
+                        for (int i = 0; i < 3; i++) 
+                        {
+                            pos = linha.find(";");
+                            dados_cargos[i] = linha.substr(0, pos); // cria uma substring ate o ; e copia para o vetor auxiliar
+                            linha.erase(0, pos + 1); // apagar o que ja foi extraido
+                        }
+                    int id_cargo_aux = stoi(dados_cargos[0]); // converter o ID de string para int
+                    float salario_aux = stof(dados_cargos[2]);// converter o salario de string para float
+                    Cargo cargo_aux(id_cargo_aux, dados_cargos[1], salario_aux);
+                    cargos.push_back(cargo_aux); 
+                    }
+                    arquivo.close();
+                    cout << "Dados de cargos carregados com sucesso!\n";
+                }
+                else 
+                {
+                    cout << "Erro ao abrir o arquivo\n";
+                }   
+            }
+
+            int getID_Cargo() const 
+            { 
+                return id_cargo; 
+            }
+
+            string getNome() const 
+            { 
+                return nome; 
+            }
+
+            float getSalario() const 
+            { 
+                return salarioBase; 
+            }      
+    };
   
     class Colaborador
     { 
@@ -20,12 +93,12 @@ using namespace std;
             static int contador_ID;
             int id;
             string nome;
-            string cargo;
+            Cargo objetoCargo;
             string cpf;
             //foto - definir como fazer;            
             string sexo;
             struct tm data_admissao; // struct padrao da biblioteca ctime
-            struct tm data_desligamento; // struct padrao da biblioteca ctime     
+            struct tm data_desligamento; // struct padrao da biblioteca ctime
 
         public:
 
@@ -33,7 +106,6 @@ using namespace std;
             {
                 this->id = ++contador_ID; // atribui id e soma 1 ao contador (id sequencial do sistema)
                 this->nome = "Nao preenchido";
-                this->cargo = "Nao preenchido";
                 this->cpf = "00000000000";
                 this->sexo = "Nao preenchido";
                 struct tm data_admissao = {0};
@@ -44,7 +116,7 @@ using namespace std;
             {
                 this->id = id_arquivo;
                 this->nome = nome_arq;
-                this->cargo = cargo_arq;
+                //this->cargo = cargo_arq;
                 this->cpf = cpf_arq;
                 this->sexo = sexo_arq;
 
@@ -52,11 +124,22 @@ using namespace std;
                 contador_ID = id_arquivo;
                 }
             }
+
+            string getNomeCargo() const
+            {
+                return objetoCargo.getNome();
+            }
+
+            float getSalarioBase() const
+            {
+                return objetoCargo.getSalario();                
+            }
             
-            void addColaborador()
+            void addColaborador(vector <Cargo>& listaCargos)
             {
                 string nome_Temp, cargo_Temp, sexo_Temp, cpf_Temp; // variaveis temporarias para guardar entradas do usuario
-               
+                int cargo_aux;
+
                 // Lógica para obter a data atual e salvar no membro data de admissao
                 time_t tempoBruto = time(0); // Pega o tempo agora
                 struct tm* dataAtual = localtime(&tempoBruto); // Converte para struct tm
@@ -68,22 +151,35 @@ using namespace std;
                 cout << "=====Cadastro de novo colaborador=====" << endl;
                 cout << "Digite o Nome: ";
                 getline(cin >> ws, nome_Temp); // 'ws' limpa o buffer para aceitar espaços
+
                 cout << "Digite o CPF (apenas numeros): ";
                 cin >> cpf_Temp;
+
                 cout << "Digite o Sexo (M/F): ";
                 cin >> sexo_Temp;
-                while(sexo_Temp != "m" && sexo_Temp != "M" && sexo_Temp != "f" && sexo_Temp != "F"){
+                while(sexo_Temp != "m" && sexo_Temp != "M" && sexo_Temp != "f" && sexo_Temp != "F")
+                {
                     cout << "Informacao invalida! Digite M ou F novamente.\n";
                     cin >> sexo_Temp;
                 }
-                transform(sexo_Temp.begin(), sexo_Temp.end(), sexo_Temp.begin(), ::toupper);
-                cout << "Digite o Cargo: ";
-                getline(cin >> ws, cargo_Temp); // 'ws' limpa o buffer para aceitar espaços
-              
+                transform(sexo_Temp.begin(), sexo_Temp.end(), sexo_Temp.begin(), ::toupper); // deixando a letra maiuscula
+
+                cout << "Digite o numero correspondente ao cargo: \n";
+                for (int i = 0; i < listaCargos.size(); i++) 
+                {
+                    cout << listaCargos[i].getID_Cargo() << ". " << listaCargos[i].getNome() << endl;
+                }
+                cin >> cargo_aux;
+                while(cargo_aux < 1 || cargo_aux > listaCargos.size()) 
+                {
+                    cout << "Cargo invalido! Escolha entre 1 e " << listaCargos.size() << ": ";
+                    cin >> cargo_aux;
+                }
+
                 this->nome = nome_Temp;
                 this->cpf = cpf_Temp;
                 this->sexo = sexo_Temp;
-                this->cargo = cargo_Temp;
+                this->objetoCargo = listaCargos[cargo_aux - 1]; // - 1 pois o vetor de cargos comeca a contar em zero
 
                 cout << "Colaborador " << this->nome << " com ID " << this->id << " cadastrado com sucesso. Admissao em: " << data_admissao_aux << endl;
             }
@@ -108,10 +204,10 @@ using namespace std;
                 return nome;
             }
 
-            string getCargo() const
-            {
-                return cargo;
-            }
+            //string getCargo() const
+            //{
+            //    return cargo;
+            //}
 
             string getCPF() const
             {
@@ -126,6 +222,17 @@ using namespace std;
 
     int Colaborador::contador_ID = 0; //inicializa o contador de IDs
 
+    //class ColaboradorMensalista :: public Colaborador
+    //{
+        //desenvolver - uso de heranca (requisito do projeto)
+    //}
+
+    //class ColaboradorHorista :: public Colaborador
+    //{
+        //desenvolver - uso de heranca (requisito do projeto)
+
+    //}
+
     class FolhaPagamento
     {
            
@@ -137,7 +244,7 @@ using namespace std;
                    
         public:
             
-           void enviaFinanceiro()
+           void enviaFinanceiro() // inicialmente vai apenas gerar um arquivo csv com os dados e na segunda etapa envia para o financeiro
             {
                 return;
             }
@@ -149,38 +256,37 @@ using namespace std;
                        
             float calculaBruto(Colaborador& colaboradorA) //Acesso indireto via método público
             {
-                return 0;
+                return 0; 
             }
                   
-            float calculaLiquido()
+            float calculaLiquido(Colaborador& colaboradorA)
             {
                 return 0;
             }
     };
 
-    // classe para salvar/pegar dados de colaboradores em arquivo txt / csv ou banco de dados (implementacao futura)
+    // classe para salvar/pegar dados de colaboradores em arquivo csv ou banco de dados (implementacao futura)
     class BancoDados_RH
     {
         public:
 
-            // salva em arquivo txt/csv
-            void salvarArquivo(const vector <Colaborador>& colaboradores) // acessa as informacoes de um vetor de objetos do tipo Colaborador
+            // salva em arquivo csv
+            void exportarPlanilha(const vector <Colaborador>& colaboradores) // acessa as informacoes de um vetor de objetos do tipo Colaborador
             {
-                ofstream arquivo("ColaboradoresRH.csv"); // ofstream escreve dados no arquivo
-                if (arquivo.is_open()) // confere se o arquivo esta aberto
+                ofstream planilha("ColaboradoresRH.csv"); // ofstream escreve dados no arquivo
+                if (planilha.is_open()) // confere se o arquivo esta aberto
                 {
-                    arquivo << "ID;Nome;Cargo;CPF;Sexo" << endl; // cabecalho do arquivo
+                    planilha << "ID;Nome;Cargo;CPF;Sexo" << endl; // cabecalho do arquivo
 
                     for (int i = 0; i < colaboradores.size(); i++) // loop roda o mesmo numero de vezes que o numero de colaboradores que podem ser cadastrados no vetor
                     {
-                        arquivo << colaboradores[i].getID() << ";";
-                        arquivo << colaboradores[i].getNome() << ";";
-                        arquivo << colaboradores[i].getCargo() << ";";
-                        arquivo << colaboradores[i].getCPF() << ";";
-                        arquivo << colaboradores[i].getSexo() << endl;
-
-                        }
-                    arquivo.close();    
+                        planilha << colaboradores[i].getID() << ";";
+                        planilha << colaboradores[i].getNome() << ";";
+                        planilha << colaboradores[i].getNomeCargo() << ";";
+                        planilha << colaboradores[i].getCPF() << ";";
+                        planilha << colaboradores[i].getSexo() << endl;
+                    }
+                    planilha.close();    
                     cout << "Dados salvos com sucesso!\n";
                 } 
 
@@ -190,7 +296,7 @@ using namespace std;
                 }
 }
 
-            void carregarArquivo(vector <Colaborador>& colaboradores) // pega informacoes em arquivo txt/csv TERMINAR DE DESENVOLVER
+            void carregarPlanilha(vector <Colaborador>& colaboradores) // pega informacoes em arquivo csv 
             {
                 ifstream arquivo("ColaboradoresRH.csv"); // ifstream le dados no arquivo
                 if (arquivo.is_open()) // confere se o arquivo esta aberto
@@ -215,19 +321,19 @@ using namespace std;
                     colaboradores.push_back(colab_aux); // atribui os dados de colab_aux para o vetor colaboradores
                     }
                     arquivo.close();
-                    cout << "Dados carregados com sucesso!\n";
+                    cout << "Dados de colaboradores carregados com sucesso!\n";
                 }
                 else {
                     cout << "Erro ao abrir o arquivo\n";
                 }
 }
 
-            void salvarBanco() // segunda avaliacao
+            void salvarBanco() // metodo a ser desenvolvido na segunda avaliacao
             {
                 return;
             }
 
-            void carregarBanco() // segunda avaliacao
+            void carregarBanco() // metodo a ser desenvolvido na segunda avaliacao
             {
                 return;
             }
@@ -237,12 +343,14 @@ using namespace std;
 int main() {
    
     int menu_aux = 0, addColaborador_aux = 0; 
-    string linha;
     vector <FolhaPagamento> folhas_pagamento; // vetor de objetos do tipo FolhaPagamento
     vector <Colaborador> colaboradores; // vetor de objetos do tipo Colaborador
+    vector <Cargo> listaCargos;
+    Cargo cargos_aux; // vetor para armazenar cargos do arquivo csv
     BancoDados_RH arquivo;
-    arquivo.carregarArquivo(colaboradores); // puxa os dados de colaboradores cadastrados anteriormente no arquivo
 
+    arquivo.carregarPlanilha(colaboradores); // puxa os dados de colaboradores cadastrados anteriormente no arquivo
+    cargos_aux.extrairCargosPlanilha(listaCargos);
     // Menu de opcoes para o usuario
     do {
     cout << "Bem-vindo ao sistema de Recursos Humanos!" << endl;
@@ -251,38 +359,23 @@ int main() {
     cout << "5. Buscar por colaborador(es)" << endl << "6. Listar horas trabalhadas" << endl << "7. Sair" << endl;
     cin >> menu_aux;
 
-    // TESTE DO carregarArquivo - apagar no final
-    //arquivo.carregarArquivo(colaboradores);
-
-    //for(int j = 0; j < colaboradores.size(); j++){
-    //    cout << colaboradores[j].getID() << "\n";
-    //    cout << colaboradores[j].getNome() << "\n";
-    //    cout << colaboradores[j].getCargo() << "\n";
-    //    cout << colaboradores[j].getCPF() << "\n";
-    //    cout << colaboradores[j].getSexo() << "\n";
-    //}
-
-    //if(colaboradores.empty())
-    //{
-    //    cout << "Nenhum colaborador cadastrado no arquivo!\n";
-    //}
-
-
     switch (menu_aux)
     {
         case 1:
-            
+
+            cout << "\n";
             cout << "Digite o numero de colaboradores que voce quer cadastrar: " << endl;
             cin >> addColaborador_aux;
             
             for(int i = 0; i < addColaborador_aux; i++)
             {
                 Colaborador novo; // Cria um novo objeto na memória
-                novo.addColaborador(); // Chama o método para preencher os dados
+                novo.addColaborador(listaCargos); // Chama o método para preencher os dados
                 colaboradores.push_back(novo); // Adiciona o objeto preenchido ao vetor
             }
         
-            arquivo.salvarArquivo(colaboradores);
+            arquivo.exportarPlanilha(colaboradores);
+            cout << "\n";
 
         break;
 
@@ -296,21 +389,21 @@ int main() {
         
         case 4:
 
-            cout << "\n\n";
+            cout << "\n";
+            cout << "=====Colaboradores cadastrados=====\n";
             for(int j = 0; j < colaboradores.size(); j++)
             {
                 cout << colaboradores[j].getID() << " || ";
                 cout << colaboradores[j].getNome() << " || ";
-                cout << colaboradores[j].getCargo() << " || ";
+                cout << colaboradores[j].getNomeCargo() << " || ";
                 cout << colaboradores[j].getCPF() << " || ";
                 cout << colaboradores[j].getSexo() << "\n";
             }
-            cout << "\n\n";
-
             if(colaboradores.empty())
             {
-                cout << "Nenhum colaborador cadastrado no arquivo!\n";
+                cout << "Nenhum colaborador cadastrado!\n";
             }
+            cout << "\n";
 
         break;
 
